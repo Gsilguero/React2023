b22ac27c4b2860ee7308a246aa7c3eaefe1dd5e7
Gsilguero <g.silguero@outlook.com>
2023-03-16 16:57:56 -0300

    subiendo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='ContenedorTotal'>
+
+
+      <h1 color='blue'>Aca vas a encontrar una breve reseña</h1>
+      
+      <h2 color='red'>
+        <button color='green'> 
+          a
+        </button> 
+        <Button />
+      </h2>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Button/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const button = () => {
+return <Button>Click aqui</Button>;     (
+    
+    <div>index</div>
+
+    )
+
+
+}
+export default index
